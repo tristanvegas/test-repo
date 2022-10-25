cf8d057c0b706c632421608622b13323871fa693
John Arnulfo Navera <john.arnulfo@foxholeqa.com>
2022-10-26 06:59:04 +0800

    testimonial & carousel: adjusted autoplay

--- resources/js/testimonial.js
+++ resources/js/testimonial.js
@@ -1,52 +1,65 @@
+$(document).ready(()=>{
 
-$(".testimonial-prev").click(function () {
-    const prev = $(".card-prev");
-    const active = $(".card-active");
-    const next = $(".card-next");
-    next.addClass("card-hide");
-    next.removeClass("card-next");
+    let timer = setInterval(()=>{
+        testimonialNext();
+    }, 10000);
 
-    active.addClass("card-next");
-    active.removeClass("card-active");
+    $(".testimonial-prev").click(function () {
+        const prev = $(".card-prev");
+        const active = $(".card-active");
+        const next = $(".card-next");
+        next.addClass("card-hide");
+        next.removeClass("card-next");
 
-    prev.addClass("card-active");
-    prev.removeClass("card-prev");
+        active.addClass("card-next");
+        active.removeClass("card-active");
 
-    if (prev.prev().html() == undefined) {
-        $(".testimonial-card:last-child").addClass("card-prev");
-        $(".testimonial-card:last-child").removeClass("card-hide");
-    } else {
-        prev.prev().addClass("card-prev");
-        prev.prev().removeClass("card-hide");
+        prev.addClass("card-active");
+        prev.removeClass("card-prev");
+
+        if (prev.prev().html() == undefined) {
+            $(".testimonial-card:last-child").addClass("card-prev");
+            $(".testimonial-card:last-child").removeClass("card-hide");
+        } else {
+            prev.prev().addClass("card-prev");
+            prev.prev().removeClass("card-hide");
+        }
+
+        clearInterval(timer);
+        timer = setInterval(()=>{
+            testimonialNext();
+        }, 10000);
+    });
+    $(".testimonial-next").click(function () {
+        testimonialNext();
+
+        clearInterval(timer);
+        timer = setInterval(()=>{
+            testimonialNext();
+        }, 10000);
+    });
+
+    function testimonialNext () {
+        const prev = $(".card-prev");
+        const active = $(".card-active");
+        const next = $(".card-next");
+
+        prev.addClass("card-hide");
+        prev.removeClass("card-prev");
+
+        active.addClass("card-prev");
+        active.removeClass("card-active");
+
+        next.addClass("card-active");
+        next.removeClass("card-next");
+
+        if (next.next().html() == undefined) {
+            $(".testimonial-card:first-child").addClass("card-next");
+            $(".testimonial-card:first-child").removeClass("card-hide");
+        } else {
+            next.next().addClass("card-next");
+            next.next().removeClass("card-hide");
+        }
+
     }
 });
-$(".testimonial-next").click(function () {
-    testimonialNext();
-});
-window.setInterval(function(){
-    testimonialNext();
-}, 10000);
-
-function testimonialNext () {
-    const prev = $(".card-prev");
-    const active = $(".card-active");
-    const next = $(".card-next");
-
-    prev.addClass("card-hide");
-    prev.removeClass("card-prev");
-
-    active.addClass("card-prev");
-    active.removeClass("card-active");
-
-    next.addClass("card-active");
-    next.removeClass("card-next");
-
-    if (next.next().html() == undefined) {
-        $(".testimonial-card:first-child").addClass("card-next");
-        $(".testimonial-card:first-child").removeClass("card-hide");
-    } else {
-        next.next().addClass("card-next");
-        next.next().removeClass("card-hide");
-    }
-
-}
