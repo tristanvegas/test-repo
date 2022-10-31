e589b004c0848740a237669ce2ca36c63e00a4f6
Tristan Vegas <106118846+tristanvegas@users.noreply.github.com>
2022-11-01 04:16:17 +0800

    update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {return view('welcome');});
+Route::get('/', function () {return view('services');});
 
 Route::get('/services', function () {
     return view('services');
